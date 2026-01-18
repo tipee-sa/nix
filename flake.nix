c7c657bb2ef27d981f778e30a7061414940b952e
Bastien Clément <bastien.clement@tipee.ch>
2026-01-18 22:47:25 +0100

    First commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "tipee SA Nix Packages";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    {
+      overlays.default = final: prev: {
+        cockroachdb = final.callPackage ./pkgs/cockroachdb.nix { };
+      };
+
+      packages =
+        let
+          systems = [
+            "x86_64-linux"
+            "aarch64-linux"
+            "aarch64-darwin"
+          ];
+        in
+        nixpkgs.lib.genAttrs systems (
+          system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ self.overlays.default ];
+            };
+          in
+          {
+            inherit (pkgs) cockroachdb;
+          }
+        );
+    };
+}
