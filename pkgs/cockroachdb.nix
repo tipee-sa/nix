69eb4b239e7b80973c97b0dc42c7e23e134dec16
sducamp <sducamp@gmail.com>
2026-03-17 15:49:21 +0100

    Refactor CockroachDB package to separate Linux and Darwin builds

--- pkgs/cockroachdb.nix
+++ pkgs/cockroachdb.nix
@@ -20,7 +20,7 @@ let
       url = "https://binaries.cockroachdb.com/cockroach-v${version}.linux-arm64.tgz";
       hash = "sha256-1DyjEcmrV/sF/aIKfypBg9B7cN1HNVTME0aYodEduBg=";
     };
-    aarch64-darwin = {
+    aarch64-darwin = fetchzip {
       url = "https://binaries.cockroachdb.com/cockroach-v${version}.darwin-11.0-arm64.tgz";
       hash = "sha256-xBDdvQQZAWWqt3/KVzQg1RyX0Bf5RlrdduPz9jKTZoE=";
     };
@@ -28,23 +28,41 @@ let
   src =
     srcs.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 
-  dontBuild = true;
-
 in
-buildFHSEnv {
-  inherit pname version;
+if stdenv.hostPlatform.isLinux then
+  buildFHSEnv {
+    inherit pname version;
 
-  runScript = "${src}/cockroach";
+    runScript = "${src}/cockroach";
 
-  extraInstallCommands = ''
-    cp -P $out/bin/cockroachdb $out/bin/cockroach
-  '';
+    extraInstallCommands = ''
+      cp -P $out/bin/cockroachdb $out/bin/cockroach
+    '';
 
-  meta = {
-    platforms = [
-      "aarch64-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-    ];
-  };
-}
+    meta = {
+      platforms = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+    };
+  }
+else
+  stdenv.mkDerivation {
+    inherit pname version src;
+
+    dontBuild = true;
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp cockroach $out/bin/cockroach
+      if [ -d lib ]; then
+        cp -r lib $out/lib
+      fi
+    '';
+
+    meta = {
+      platforms = [
+        "aarch64-darwin"
+      ];
+    };
+  }
